ffa22defdc46c8050f69cff5308ee48bcc642adb
vinaygoyal355 <vg83680@gmail.com>
2019-07-17 12:17:21 +0530

    Completed upto main screen
    Login
    SignUp

--- app/src/main/java/com/example/ac_twitterclone/TwitterUsers.java
+++ app/src/main/java/com/example/ac_twitterclone/TwitterUsers.java
@@ -4,16 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.ParseInstallation;
-
-public class MainActivity extends AppCompatActivity {
+public class TwitterUsers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ParseInstallation.getCurrentInstallation().saveInBackground();
-
+        setContentView(R.layout.activity_twitter_users);
     }
 }
