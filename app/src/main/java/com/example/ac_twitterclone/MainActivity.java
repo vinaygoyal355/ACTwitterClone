5ddb8f25608949e742c19a956293f296d022fa80
vinaygoyal355 <vg83680@gmail.com>
2019-07-17 11:54:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ac_twitterclone/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ac_twitterclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseInstallation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+    }
+}
